cf023413c26dfef400b7d9ac27dd95584846f73b
fatih <fsenyigit8@gmail.com>
2024-05-31 21:26:07 +0300

    store

--- src/redux/loginSlice.jsx
+++ src/redux/loginSlice.jsx
@@ -1,23 +1,26 @@
-
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user:{
-        email:'', password:''
-    }
-}
+  user: {
+    email: "",
+    password: "",
+  },
+};
 
 export const loginSlice = createSlice({
-    name:'login',
-    initialState,
-    reducers: {
-        // setUser: ()=> {} 
-        setUser: function (state, action) {
-            state.user = action.payload
-        }
-    }
-})
+  name: "login",
+  initialState,
+  reducers: {
+    // setUser: ()=> {}
+    setUser: function (state, action) {
+      state.user = action.payload;
+    },
 
-export const {setUser} = loginSlice.actions
-export default loginSlice.reducer
+    clearUser: (state) => {
+      state.user = { email: "", password: "" };
+    },
+  },
+});
+
+export const { setUser, clearUser } = loginSlice.actions;
+export default loginSlice.reducer;
